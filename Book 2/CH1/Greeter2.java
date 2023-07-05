8578190c8c49df8813a773254c9eaa614a7bbfa2
dhicks6 <dhicks23@msudenver.edu>
2023-07-05 13:41:23 -0600

    Created the Greeter2.java class and updated the HelloApp2.java class

---
+++ Book 2/CH1/Greeter2.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Greeter2 {
+   public void sayHello()
+   {
+    JOptionPane.showMessageDialog(null, "Hello World!", "Greeter", JOptionPane.INFORMATION_MESSAGE);
+   } 
+}
--- Book 2/CH1/HelloApp2.java
+++ Book 2/CH1/HelloApp2.java
@@ -3,9 +3,13 @@
 // Greeter class and then calling the Greeter
 // object's sayHello method
 
+// I am updating the method call to the new Greeter2 class.
+// This shows that the HelloApp2 class does not need to know what is in the Greeter
+// class so i could have just updated the Greeter.java class instead of making the Greeter2.java class
+// I have done it this way so that I can keep both classes to look back on
 public class HelloApp2 {
     public static void main(String[] args) {
-        Greeter myGreeterObject = new Greeter();
+        Greeter2 myGreeterObject = new Greeter2();
         myGreeterObject.sayHello();
     }
 }
