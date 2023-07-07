30ea3326d0ba8489795fdfeaed7637edc75ac673
dhicks6 <dhicks23@msudenver.edu>
2023-07-07 13:32:14 -0600

    Renaming directories, and created a directory for book 7

---
+++ Book 2 Programming Basics/CH2/HelloApp.java
@@ -0,0 +1,9 @@
+// This is an example of using a class variable helloMessage
+// in the class body of HelloApp
+public class HelloApp {
+    static String helloMessage;
+    public static void main(String[] args) {
+        helloMessage = "Hello, World!";
+        System.out.println(helloMessage);
+    }
+}
